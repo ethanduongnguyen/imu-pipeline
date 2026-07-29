6b287a80843c4845be7a4c202571697946f11f57
Ethan Nguyen <nguyen.ethand20@gmail.com>
2026-07-28 22:41:20 -0400

    Construct scalable framework to add filters and created a class for a moving average filter.

--- python/filters.py
+++ python/filters.py
@@ -1,4 +1,45 @@
 import numpy as np
+from abc import ABC, abstractmethod
+from collections import deque
 
-class IMUFilter:
+class BaseFilter(ABC):
+    @abstractmethod
+    def update(self, measurement: np.ndarray) -> np.ndarray:
+        pass
     
+    @abstractmethod
+    def reset(self):
+        pass
+    
+    def __call__(self, measurement):
+        return self.update(measurement)
+    
+    # Allow filters to be applied for offline processing, i.e. data from a loaded CSV file
+    def apply(self, data: np.ndarray) -> np.ndarray:
+        self.reset()
+        is_1d = data.ndim == 1
+        if is_1d:
+            data_2d = data[:, np.newaxis]
+        else:
+            data_2d = data
+            
+        filtered_results = []
+        for sample in data_2d:
+            filtered_results.append(self.update(sample))
+        
+        result = np.array(filtered_results)
+        return result.flatten() if is_1d else result 
+    
+class MovingAverage(BaseFilter):
+    def __init__(self, window_size: int = 5, num_channels: int = 3):
+        self.window_size = window_size
+        self.num_channels = num_channels
+        self.buffer = deque(maxlen=window_size)
+        self.reset()
+        
+    def reset(self):
+        self.buffer.clear()
+        
+    def update(self, measurement: np.ndarray) -> np.ndarray:
+        self.buffer.append(measurement)
+        return np.mean(self.buffer, axis = 0)
